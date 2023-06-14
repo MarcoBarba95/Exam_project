d39e4bbe74411fd945f0b2f8f1b9fafbf622667a
MarcoBarba95 <marcobarbagallo95@gmail.com>
2023-06-14 13:49:12 +0200

    Aggiunte le classi

---
+++ Personale.java
@@ -0,0 +1,34 @@
+public class Personale extends Persona{
+    private int NumBadge;
+    private String Mansione;
+
+    public Personale(String nome, String cognome, int eta, char sesso, int numBadge, String mansione) {
+        super(nome, cognome, eta, sesso);
+        this.NumBadge = numBadge;
+        this.Mansione = mansione;
+    }
+
+    public int getNumBadge() {
+        return NumBadge;
+    }
+
+    public void setNumBadge(int numBadge) {
+        NumBadge = numBadge;
+    }
+
+    public String getMansione() {
+        return Mansione;
+    }
+
+    public void setMansione(String mansione) {
+        Mansione = mansione;
+    }
+
+    @Override
+    public String toString() {
+        return "Personale{" +
+                "NumBadge=" + NumBadge +
+                ", Mansione='" + Mansione + '\'' +
+                '}';
+    }
+}
---
+++ Visitatore.java
@@ -0,0 +1,46 @@
+public class Visitatore extends Persona{
+    private String NumDoc;
+    private String IDBiglietto;
+    private String TipologiaVisita;
+
+    public Visitatore(String nome, String cognome, int eta, char sesso, String numDoc, String IDBiglietto, String tipologiaVisita) {
+        super(nome, cognome, eta, sesso);
+        this.NumDoc = numDoc;
+        this.IDBiglietto = IDBiglietto;
+        this.TipologiaVisita = tipologiaVisita;
+
+    }
+
+    public String getNumDoc() {
+        return NumDoc;
+    }
+
+    public void setNumDoc(String numDoc) {
+        NumDoc = numDoc;
+    }
+
+    public String getIDBiglietto() {
+        return IDBiglietto;
+    }
+
+    public void setIDBiglietto(String IDBiglietto) {
+        this.IDBiglietto = IDBiglietto;
+    }
+
+    public String getTipologiaVisita() {
+        return TipologiaVisita;
+    }
+
+    public void setTipologiaVisita(String tipologiaVisita) {
+        TipologiaVisita = tipologiaVisita;
+    }
+
+    @Override
+    public String toString() {
+        return "Visitatore{" +
+                "NumDoc='" + NumDoc + '\'' +
+                ", IDBiglietto='" + IDBiglietto + '\'' +
+                ", TipologiaVisita='" + TipologiaVisita + '\'' +
+                '}';
+    }
+}
